c59c601a0373040fe24cf7382be9cd3ffe1b02d9
Deh410 <aearahawa@gmail.com>
2019-09-26 23:30:08 -0300

    add controller events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'event/create'
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/features/create_event_spec.rb
+++ spec/features/create_event_spec.rb
@@ -3,23 +3,24 @@ require 'rails_helper.rb'
 # Aqui eu escrevo os passos dos testes
 
 feature 'Criar Evento' do
-    scenario 'Consegue abrir tela de eventos'
-    # Visitar rota de evento
-    visit '/events'
-    # Clicar em criar em criar evento
-    click_link 'Criar Evento'
-    # Preencher com as informacoes
-    fill_in 'NomeEvento', with: 'nome'
-    fill_in 'Data', with: 'data'
-    fill_in 'DataAplicacao', with: 'data_aplicacao'
-    fill_in 'Capacidade', with: 'capacidade'
-    fill_in 'Descricao', with: 'descricao'
-    # Clicar no botao de cadastrar evento
-    click_link 'Cadastrar Evento'
-    # Esperar que a pagina tenha o conteudo enviado
-    expect(page).to have_content('nome')
-    expect(page).to have_content('data')
-    expect(page).to have_content('data_aplicacao')
-    expect(page).to have_content('capacidade')
-    expect(page).to have_content('descricao')
+    scenario 'Consegue abrir tela de eventos' do
+        # Visitar rota de evento
+        visit '/events'
+        # Clicar em criar em criar evento
+        click_link 'Criar Evento'
+        # Preencher com as informacoes
+        fill_in 'NomeEvento', with: 'nome'
+        fill_in 'Data', with: 'data'
+        fill_in 'DataAplicacao', with: 'data_aplicacao'
+        fill_in 'Capacidade', with: 'capacidade'
+        fill_in 'Descricao', with: 'descricao'
+        # Clicar no botao de cadastrar evento
+        click_link 'Cadastrar Evento'
+        # Esperar que a pagina tenha o conteudo enviado
+        expect(page).to have_content('nome')
+        expect(page).to have_content('data')
+        expect(page).to have_content('data_aplicacao')
+        expect(page).to have_content('capacidade')
+        expect(page).to have_content('descricao')
+    end
 end
