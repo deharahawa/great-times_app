2773b943f7017acdde4ee88133fec0c39d2bab6e
Deh410 <aearahawa@gmail.com>
2019-10-08 13:48:20 -0300

    update application css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
