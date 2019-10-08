94627cf557b778e405fd86ef57f2b46e5be08848
Deh410 <aearahawa@gmail.com>
2019-10-08 13:34:53 -0300

    fix bootstrap rocket

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
