82cb7024a03fbccd431cf5894e76ef84f6dfdde9
Deh410 <aearahawa@gmail.com>
2019-10-08 13:56:17 -0300

    update application css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,2 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap-sprockets
-//= require_tree .
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ require("channels")
 
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require_tree .
 
 
