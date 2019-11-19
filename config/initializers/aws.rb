65b565c8f4393146546c23b4e524c9d3a1ab00bb
deh410 <aearahawa@gmail.com>
2019-11-19 19:58:44 -0300

    add aws config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -9,7 +9,7 @@ s3 = Aws::S3::Resource.new
 
 Aws.config.update({
     region: 'us-west-1',
-    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+    credentials: Aws::Credentials.new('ASIAXF6QHR5PCWTXS4TK', '91AmvM7WRNA/2D62e6o+Oj4F8i2yJATHaIxEZLSG')
   })
 
  obj = s3.bucket('stage-greattimes-app')
